4cc2387ec5bbe3c189a27531994355c6e0106dbb
Rasheed19 <ibraheem.abdulrasheed@gmail.com>
2023-08-19 20:07:45 +0100

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rrct",
-    version="1.0.2",
+    version="1.0.3",
     author="Rasheed Ibraheem",
     author_email="R.O.Ibraheem@sms.ed.ac.uk",
     maintainer="Rasheed Ibraheem",
